74f66bfe518ab837ea26314b1cbc36cbe27891ba
Nhan Vu <nhan997@gmail.com>
2020-03-31 01:20:21 +0200

    add header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import {HeaderModule} from './shared/components';
 import { AuthGuardService } from './shared/services';
 import { HomeComponent } from './layouts/pages/home/home.component';
 import { DxDataGridModule, DxFormModule } from 'devextreme-angular';
+import {PropertySidebarListComponent} from './layouts/pages/property-sidebar-list/property-sidebar-list.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [ AuthGuardService ]
+  },
+  {
+    path: 'property-list',
+    component: PropertySidebarListComponent,
     canActivate: [ AuthGuardService ]
   },
   {
--- src/app/layouts/layout.module.ts
+++ src/app/layouts/layout.module.ts
@@ -3,13 +3,11 @@ import {BannerModule, FooterModule, HeaderModule} from '../shared/components';
 import { HomeComponent } from './pages/home/home.component';
 import {SearchBoxModule} from '../shared/components/search-box/search-box.component';
 import {PropertyBoxModule} from '../shared/components/property-box/property-box.component';
-import {PropertyListComponent} from './pages/property-list/property-list.component';
 import {PropertySidebarListComponent} from './pages/property-sidebar-list/property-sidebar-list.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    PropertyListComponent,
     PropertySidebarListComponent
   ],
   imports: [
@@ -21,7 +19,6 @@ import {PropertySidebarListComponent} from './pages/property-sidebar-list/proper
   ],
   exports: [
     HomeComponent,
-    PropertyListComponent,
     PropertySidebarListComponent
   ]
 })
